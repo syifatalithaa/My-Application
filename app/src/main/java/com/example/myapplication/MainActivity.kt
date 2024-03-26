2ec87d5966694ea7d7f12da4b1fa3387a88791c9
Syifa Ayu Talitha M <syifaayutalitha19@gmail.com>
2024-03-26 19:14:36 +0700

    init project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets }
+            val textView = findViewById<TextView>(R.id.tv_hello)
+            val button = findViewById<Button>(R.id.btn_tombol)
+            button.setOnClickListener {
+                textView.text = "Next"
+                }
+        }
+        }
+
+
+
